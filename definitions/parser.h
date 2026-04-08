901d3848eb6dd4fdba0481c2593bde0fe55099db
NBnY5A <victor.lito@a.ucb.br>
2026-04-08 17:54:56 -0300

    feat: Define the functions that will be used by AST

---
+++ definitions/parser.h
@@ -0,0 +1,43 @@
+#include "./lexer.h"
+
+typedef struct ASTNode
+{
+    int type;
+    char *value;
+    struct ASTNode *left;
+    struct ASTNode *right;
+} ASTNode;
+
+ASTNode *createNode(int type, char *value);
+
+void freeNode(ASTNode *node);
+
+ASTNode *parseConditional(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseRepetitive(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseFactor(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseTerm(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseSimpleExpression(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseExpression(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseAssignment(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseStatement(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseCompoundStatement(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseIdentifierList(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseDeclaration(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseVarDeclaration(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseBlock(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseProgram(HashTable *table, Entry **currentEntry);
+
+ASTNode *parseTokens(HashTable *table);
